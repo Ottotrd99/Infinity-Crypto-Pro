d4da9c8a1785207a8fed33a87db93091439af3c4
Ottotrd99 <otonidaytrade@gmail.com>
2026-04-16 13:59:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ col_side, col_main = st.columns([1, 5])
 
 with col_side:
     st.markdown("<div class='glass-panel'>", unsafe_allow_html=True)
-    tf = st.selectbox("TEMPO:", ["1m", "5m", "15m"], index=1)
+    tf = st.selectbox("TEMPO:", ["1m", "5m", "15m", "1h], index=1)
     if st.button("⚡ SCANNER"):
         st.session_state['setups'] = buscar_setups()
 
